adf69c14aa15aaca1e1f7922fac63edc292c6b9e
gismo8992 <gismo8992@mail.ru>
2023-01-29 18:57:04 +0300

    Выполнение правок первого упражнения.

--- src/main/java/org/example/practice1/Practice1.java
+++ src/main/java/org/example/practice1/Practice1.java
@@ -2,6 +2,6 @@ package org.example.practice1;
 
 public class Practice1 {
     public static void main(String[] args) {
-        System.out.println("Solving the 1-st task");
+        System.out.println("Solving the 1-st task!");
     }
 }
