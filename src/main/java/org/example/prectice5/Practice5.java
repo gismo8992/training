cddef2a3d897a08c73388a7731a12c677daada1c
gismo8992 <gismo8992@mail.ru>
2023-01-31 00:41:56 +0300

    Практика №5. Многопоточность, обновление.

--- src/main/java/org/example/prectice5/Practice5.java
+++ src/main/java/org/example/prectice5/Practice5.java
@@ -2,6 +2,18 @@ package org.example.prectice5;
 
 public class Practice5 {
     public static void main(String[] args) {
+        MyThread myThread1 = new MyThread();
+        MyThread myThread2 = new MyThread();
+        myThread1.start();
+        System.out.println("Hello from main thread!");
+        myThread2.start();
+    }
+}
 
+class MyThread extends Thread {
+    public void run() {
+        for(int i = 0; i <5; i++) {
+            System.out.println("Hello from MyThread " + i);
+        }
     }
 }
