8d89f3a871ad567bc39f3eb6b38d7a2f6c8626f5
gismo8992 <gismo8992@mail.ru>
2023-02-26 19:31:31 +0300

    Практика №9.Inheritance

---
+++ src/main/java/org/example/practice10/packages/Test.java
@@ -0,0 +1,9 @@
+package org.example.practice10.packages;
+
+import org.example.practice10.packages.Forest.Squirell;
+import org.example.practice10.packages.Forest.Tree;
+
+public class Test {
+    Tree tree1 = new Tree();
+    Squirell squirell1 = new Squirell();
+}
---
+++ src/main/java/org/example/practice10/packages/polimorfism/Animal.java
@@ -0,0 +1,10 @@
+package org.example.practice10.packages.polimorfism;
+
+public class Animal {
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+    public void run() {
+        System.out.println("Animal is runnig");
+    }
+}
